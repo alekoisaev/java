0dcef3b96fc3018a772e9b66be8b730538f15987
ALeko <42478187+alekoisaev@users.noreply.github.com>
2020-10-02 01:07:21 +0400

    hm2

---
+++ 10.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] nums = {33, 45, 8, 65, 98, 105, 36};
+
+        Arrays.sort(nums);
+
+        for(int num: nums) {
+            System.out.println(num);
+        }
+    }
+}
