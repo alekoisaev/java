b12375b8e8aa43f997dc0aadf9c9acf2677ff999
ALeko <42478187+alekoisaev@users.noreply.github.com>
2020-10-02 00:39:20 +0400

    hm2

---
+++ 9.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] nums = {33, 45, 8, 65, 98, 105, 36};
+
+        Arrays.sort(nums);
+
+        System.out.println("Min: " + nums[0]);
+        System.out.println("Max: " + nums[nums.length - 1]);
+    }
+}
